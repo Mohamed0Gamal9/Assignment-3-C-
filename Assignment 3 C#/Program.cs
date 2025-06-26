cf1023215370e2800aec3de8b094bd2c2731b503
Mohamed Gamal <mohamed.g.abdelmotaal@gmail.com>
2025-06-26 14:35:00 +0300

    q2 solved

--- Assignment 3 C#/Program.cs
+++ Assignment 3 C#/Program.cs
@@ -11,6 +11,16 @@
             //Console.WriteLine($"the number is : {number}"); 
             #endregion
 
+            #region Q2
+            //Console.Write("enter any number: ");
+            //int number = Convert.ToInt16(Console.ReadLine());
+            //Console.Clear();
+            //Console.WriteLine($"the number is : {number}");
+            //it will give an exception bec it can't happen(System.FormatException: 'The input string 'asd' was not in a correct format.' 
+	        #endregion  
+
+)
+
         }
     }
 }
