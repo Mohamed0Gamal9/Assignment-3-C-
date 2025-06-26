e75e23ba8c47ec02f273d62ab5d37a690f925ff1
Mohamed Gamal <mohamed.g.abdelmotaal@gmail.com>
2025-06-26 14:30:34 +0300

    Q1 solved

--- Assignment 3 C#/Program.cs
+++ Assignment 3 C#/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace assignment_3
+{
+    internal class program
+    {
+        static void Main(string[] args)
+        {
+            #region Q1
+            //Console.Write("enter any number: ");
+            //int number =Convert.ToInt16 (Console.ReadLine());
+            //Console.Clear();
+            //Console.WriteLine($"the number is : {number}"); 
+            #endregion
+
+        }
+    }
+}
